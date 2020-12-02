49ddec4752d60eabd462fbdb1be3f83a3d08aa8c
Spencer Mellor <spencermellor@MacBook-Pro-3.local>
2020-12-01 19:26:52 -0500

    added the styling to the page. ready to hand in now.

--- js/components/fetchData.js
+++ js/components/fetchData.js
@@ -1,3 +1,10 @@
+let errorCodes = {
+    404: "Not Found! Oh noes! Check your URL",
+    500: "Ya sorry can't help you... the server is just borked",
+    403: "You shall not pass! Unless you have creds. Then sure go ahead.",
+    503: "Service is unavailable! The servers are all hasing a coffee break."
+}
+
 // copied from class files
 
 async function fetchData(sourceURL) {
